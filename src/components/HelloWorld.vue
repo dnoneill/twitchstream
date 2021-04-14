6231f6f337d76b2b08fac1ce55b0e825a64d94f0
Niqui O'Neill <doneill@ncsu.edu>
2021-04-14 13:38:17 -0400

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,64 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="hello" v-if="clues.length > 0">
+    <h1>{{category}}</h1>
+    <div v-for="(clue, index) in clues" v-bind:key="clue.message">
+      <div v-if="clue.question">
+        <h1>{{clue.value}}</h1>
+        <div>{{clue.question}}</div>
+        <input v-model="input[index]" v-on:keyup.enter="checkAnswer(index)">
+        <div v-if="clue.message">{{clue.message}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    api: String
+  },
+  data: function() {
+    return { 
+      clue: '',
+      clues: [],
+      input: [],
+      message: '',
+      category: ''
+    }
+  },
+  mounted: function() {
+  },
+  methods: {
+    checkAnswer: function(index) {
+      const cleanedinput = this.input[index].toLowerCase();
+      const cleanedanswer = this.clues[index].answer.toLowerCase();
+      console.log(cleanedinput)
+      console.log(cleanedanswer)
+      if (cleanedinput == cleanedanswer){
+        this.$set(this.clues[index], 'message', 'Correct!')
+      } else {
+        this.$set(this.clues[index], 'message', 'Incorrect! Try again!')
+      }
+      console.log(this.clues[index])
+    }
+  },
+  created: function () {
+    const randomnumber = parseInt(Math.random()*100);
+    console.log(randomnumber)
+    axios.get(this.api + randomnumber).then((response) => {
+      // handle success
+      this.clues = response.data.clues;
+      this.category = response.data.title;
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
   }
 }
 </script>
